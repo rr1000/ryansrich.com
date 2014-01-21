c77419ed159363cc73c478f86f5d1c7a2f583f51
Ryan S. Rich <ryanSrich@Ryans-MacBook-Pro.local>
2014-01-20 20:01:13 -0500

    Removed fading for now, increased padding on splash image

--- v2/main.js
+++ v2/main.js
@@ -1,13 +1,18 @@
-var fadeStart=0
-	,fadeUntil=300
-	,fading = $('.fading');
+// var fadeStart=0
+// 	,fadeUntil=300
+// 	,fading = $('.fading');
 
-$(window).bind('scroll', function(){
-	var offset = $(document).scrollTop(),opacity=0;
-	if( offset<=fadeStart ){
-		opacity=1;
-	}else if( offset<=fadeUntil ){
-		opacity=1-offset/fadeUntil;
-	}
-	fading.css('opacity',opacity);
-});
+// // Only apply fading when screen size is greater than 480px
+// $(window).resize(function() {
+// 	if ($(this).width() > 480) {
+// 		$(window).bind('scroll', function(){
+// 			var offset = $(document).scrollTop(),opacity=0;
+// 			if( offset<=fadeStart ){
+// 				opacity=1;
+// 			}else if( offset<=fadeUntil ){
+// 				opacity=1-offset/fadeUntil;
+// 			}
+// 			fading.css('opacity',opacity);
+// 		});
+// 	}
+// });
