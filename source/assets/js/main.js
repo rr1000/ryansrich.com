853593f273fbdfc480b6a8e30242c626e52dcffd
Ryan Rich <rrichrs@gmail.com>
2016-07-11 16:39:11 -0400

    Updates

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -3,4 +3,36 @@ $('document').ready(function(){
     var nav = $('.main-header nav');
     var navIcon = $('.main-header .icon-nav');
     navIcon.click(function(){ nav.fadeToggle(350); });
+    // Animate
+    $("._a").animsition({
+        inClass: 'fade-in',
+        outClass: 'fade-out',
+        inDuration: 1500,
+        outDuration: 800,
+        linkElement: '.animsition-link',
+        loading: false,
+        timeout: false,
+        timeoutCountdown: 5000,
+        onLoadEvent: true,
+        browser: [ 'animation-duration', '-webkit-animation-duration'],
+        overlay : false,
+        overlayClass : 'animsition-overlay-slide',
+        overlayParentElement : 'body',
+        transition: function(url){ window.location.href = url; }
+    });
+    // Smooth scroll
+    $(function(){
+        $('a[href*="#"]:not([href="#"])').click(function(){
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length){
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 });
