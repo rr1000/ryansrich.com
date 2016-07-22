2f5544b271032de778b0ef37d79bd95e7c576ceb
Ryan Rich <rrichrs@gmail.com>
2016-07-22 13:37:22 -0700

    Finalizing content, optimizing images

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -3,23 +3,6 @@ $('document').ready(function(){
     var nav = $('.main-header nav');
     var navIcon = $('.main-header .icon-nav');
     navIcon.click(function(){ nav.fadeToggle(350); });
-    // Animate
-    $("._a").animsition({
-        inClass: 'fade-in',
-        outClass: 'fade-out',
-        inDuration: 1500,
-        outDuration: 800,
-        linkElement: '.animsition-link',
-        loading: false,
-        timeout: false,
-        timeoutCountdown: 5000,
-        onLoadEvent: true,
-        browser: [ 'animation-duration', '-webkit-animation-duration'],
-        overlay : false,
-        overlayClass : 'animsition-overlay-slide',
-        overlayParentElement : 'body',
-        transition: function(url){ window.location.href = url; }
-    });
     // Smooth scroll
     $(function(){
         $('a[href*="#"]:not([href="#"])').click(function(){
