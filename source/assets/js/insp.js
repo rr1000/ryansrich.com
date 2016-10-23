5414085ffd35fab6c96265e03ca3c1d1234d93d9
Ryan Rich <rrichrs@gmail.com>
2016-10-23 00:16:29 -0700

    Adding new albums

---
+++ source/assets/js/insp.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+    el: '#insp',
+    data:{
+        plays:[
+            {
+                albumBg: 'shadow',
+                album: 'Shadow of a Doubt',
+                link: '//itun.es/us/H8SZ-',
+                artist: 'Freddie Gibbs',
+                text: 'Shadow of a Doubt received critical acclaim from music critics. At Metacritic, which assigns a normalized rating out of 100 to reviews from mainstream critics, the album received an average score of 78, based on 12 reviews, which indicates "generally favorable reviews"',
+                source: '//en.wikipedia.org/wiki/Shadow_of_a_Doubt_(album)'
+            },
+            {
+                albumBg: 'gales',
+                album: 'Beyond the Fleeting Gales',
+                link: 'https://itun.es/us/OcAweb',
+                artist: 'Crying',
+                text: 'Crying is an American rock band from Purchase, New York.',
+                source: '//en.wikipedia.org/wiki/Crying_(band)'
+            }
+        ],
+        albums:[
+            { artist: "Joy Division", name: "Closer" },
+            { artist: "Swans", name: "The Glowing Man" }
+        ]
+    }
+})
