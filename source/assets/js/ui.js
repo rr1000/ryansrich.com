f912906fdd87d1c042f02431ed999bb9b2ff9e84
Ryan Rich <rrichrs@gmail.com>
2016-10-26 22:22:38 -0700

    Added availability badge

--- source/assets/js/ui.js
+++ source/assets/js/ui.js
@@ -1,1 +1,10 @@
 // tbd
+$('document').ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop()>0){
+            $('.badge').fadeOut();
+        }else{
+            $('.badge').fadeIn();
+        }
+    });
+});
