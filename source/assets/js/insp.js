5fd8a201bc802e92c5edc0a106c02c080c53a951
Ryan Rich <rrichrs@gmail.com>
2016-10-30 10:49:36 -0700

    Updating inspiration

--- source/assets/js/insp.js
+++ source/assets/js/insp.js
@@ -3,30 +3,32 @@ var app = new Vue({
     data:{
         plays:[
             {
-                albumBg: 'shadow',
-                album: 'Shadow of a Doubt',
-                link: '//itun.es/us/H8SZ-',
-                artist: 'Freddie Gibbs',
-                text: 'Shadow of a Doubt received critical acclaim from music critics. At Metacritic, which assigns a normalized rating out of 100 to reviews from mainstream critics, the album received an average score of 78, based on 12 reviews, which indicates "generally favorable reviews"',
-                source: '//en.wikipedia.org/wiki/Shadow_of_a_Doubt_(album)'
+                albumBg: 'fangs',
+                album: 'The Light',
+                link: '//itun.es/us/SJh0ab',
+                artist: 'Soft Fangs',
+                text: 'Raised in the outskirts of Massachusetts, now living in Brooklyn, Soft Fangs is the latest musical outlet of John Lutkevich. His songwriting, which has been compared to the likes of Elliott Smith, Sparklehorse, Radiohead and Nick Drake, combines introspective and poetic lyricism with fragile',
+                source: '//www.explodinginsoundrecords.com/soft-fangs'
             },
             {
-                albumBg: 'gales',
-                album: 'Beyond the Fleeting Gales',
-                link: 'https://itun.es/us/OcAweb',
-                artist: 'Crying',
-                text: 'Crying is an American rock band from Purchase, New York.',
-                source: '//en.wikipedia.org/wiki/Crying_(band)'
+                albumBg: 'atrocity',
+                album: 'Atrocity Exhibition',
+                link: '//itun.es/us/Mlbheb',
+                artist: 'Danny Brown',
+                text: 'Atrocity Exhibition is the fourth studio album by American hip hop recording artist Danny Brown. It was released on September 27, 2016, by Warp Records. It is primarily produced by British producer Paul White. The album features guest appearances from Kendrick Lamar, Ab-Soul, Earl Sweatshirt',
+                source: '//en.wikipedia.org/wiki/Atrocity_Exhibition_(album)'
             }
         ],
         albums:[
-            { artist: "Joy Division", name: "Closer" },
-            { artist: "Swans", name: "The Glowing Man" }
+            { artist: 'Joy Division', name: 'Closer' },
+            { artist: 'Swans', name: 'The Glowing Man' },
+            { artist: 'Freddie Gibbs', name: 'Shadow of a Doubt' },
+            { artist: 'Crying', name: 'Beyond the Fleeting Gales' }
         ],
         books:[
             {
                 class: 'icon i-multiple-signatures',
-                name: "Multiple Signatures: On Designers, Authors, Readers and Users",
+                name: 'Multiple Signatures: On Designers, Authors, Readers and Users',
                 author: "Michael Rock"
             },
             {
