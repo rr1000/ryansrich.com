59d0e64780067243556953d1aec7fa46443b0f8c
Ryan Rich <rrichrs@gmail.com>
2016-11-14 20:49:10 -0800

    9.1

--- source/assets/js/menu.js
+++
@@ -1,9 +0,0 @@
-$('document').ready(function(){
-    // Mobile menu
-    var nav = $('.main-header nav');
-    var navIcon = $('.main-header .menu');
-    navIcon.click(function(){
-        nav.fadeToggle(350);
-        $(this).toggleClass('w');
-    });
-});
