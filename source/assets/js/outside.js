33f6a82c6983ff202bcd29d8c5f9598af97448a8
Ryan Rich <rrichrs@gmail.com>
2016-11-28 20:15:31 -0800

    Adding more hikes

--- source/assets/js/outside.js
+++ source/assets/js/outside.js
@@ -28,6 +28,33 @@ var app = new Vue({
                 hikeDistance: '2.1mi',
                 hikeElevation: '795ft',
                 hikeCover: '/assets/img/covers/cover-latourell-falls-loop-trail.png'
+            },
+            {
+                hikeName: 'Wildwood Trail',
+                hikeDate: '2016-10-22',
+                hikeTrail: 'trail-wildwood-trail',
+                hikeLocation: 'Portland, OR',
+                hikeDistance: '3.2mi',
+                hikeElevation: '810ft',
+                hikeCover: '/assets/img/covers/cover-wildwood-trail.png'
+            },
+            {
+                hikeName: 'Macleay Trail',
+                hikeDate: '2016-10-08',
+                hikeTrail: 'trail-upper-macleay-park',
+                hikeLocation: 'Portland, OR',
+                hikeDistance: '5.3mi',
+                hikeElevation: '977ft',
+                hikeCover: '/assets/img/covers/cover-upper-macleay-park.png'
+            },
+            {
+                hikeName: 'Dogwood Trail',
+                hikeDate: '2016-08-21',
+                hikeTrail: 'trail-dogwood',
+                hikeLocation: 'Portland, OR',
+                hikeDistance: '2.4mi',
+                hikeElevation: '536ft',
+                hikeCover: '/assets/img/covers/cover-dogwood-trail.png'
             }
         ]
     },
